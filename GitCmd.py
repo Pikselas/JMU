b2678a73edbdfeea63728a0a25deceff829d5c18
kashikichi <60090083+Pikselas@users.noreply.github.com>
2022-07-08 11:07:37 +0530

    UR PP is not public 😁😁

--- GitCmd.py
+++ GitCmd.py
@@ -1,25 +1,25 @@
 import subprocess
 
 class GitCmd:
-    GIT_CMD_LIST = []
+    __GIT_CMD_LIST = []
    
     def __init__(self , path : str) -> None:
         self.path = path
 
     def CommitFile(self , filename : str , message : str) -> None:
-        self.GIT_CMD_LIST.append(["commit" , filename , "-m" , message])
+        self.__GIT_CMD_LIST.append(["commit" , filename , "-m" , message])
     
     def AddNewFile(self , filename : str) ->None:
-        self.GIT_CMD_LIST.append(["add" , filename])
+        self.__GIT_CMD_LIST.append(["add" , filename])
         self.CommitFile(filename , "ADDED FILE :" + filename)
 
     def Clear(self) -> None:
-        self.GIT_CMD_LIST.clear()
+        self.__GIT_CMD_LIST.clear()
 
     def Execute(self ,Executer) -> None:
         git_path = ["git" , "-C" , self.path]
-        self.GIT_CMD_LIST.append(["pull"])
-        self.GIT_CMD_LIST.append(["push"])
+        self.__GIT_CMD_LIST.append(["pull"])
+        self.__GIT_CMD_LIST.append(["push"])
         for command in self.GIT_CMD_LIST:
             cmd = git_path + command
             result = subprocess.run(cmd ,shell = True , capture_output=True , text = True)
--- ModelSystem.py
+++ ModelSystem.py
@@ -5,21 +5,21 @@ from shutil import copyfile
 
 class Modelsystem:
     
-    modelsDirectory = "jellymilk/data/models"
-    catagoriesDirectory = "jellymilk/data/catagories"
-    profilePicDirectory = "jellymilk/profile_pics"
-    catagoriesPath = "jellymilk/data/catagories.json"
+    __modelsDirectory = "jellymilk/data/models"
+    __catagoriesDirectory = "jellymilk/data/catagories"
+    __profilePicDirectory = "jellymilk/profile_pics"
+    __catagoriesPath = "jellymilk/data/catagories.json"
 
     def __init__(self , path : str) -> None:
         self.path = path
         self.GitHandler = GitCmd.GitCmd(path)
     
     def AddNewModel(self , name : str, desc : str , links : list , profile_pic : str) -> bool:
-        filePath = self.modelsDirectory + '/' + name + '.json'
+        filePath = self.__modelsDirectory + '/' + name + '.json'
         finalPath = self.path + '/' + filePath
         if not op.isfile( finalPath) : 
             ModelJson = {"description" : desc , "links" : links}
-            modelPicPath = self.profilePicDirectory + '/' + name + ".png"
+            modelPicPath = self.__profilePicDirectory + '/' + name + ".png"
             copyfile(profile_pic , self.path + '/' + modelPicPath)
             self.GitHandler.AddNewFile(modelPicPath)
             with open(finalPath , "w") as model:
@@ -29,24 +29,24 @@ class Modelsystem:
         return False
 
     def AddNewCatagory(self , catagory : str) -> bool:
-        catagoryPath = self.catagoriesDirectory + '/' + catagory + '.json'
+        catagoryPath = self.__catagoriesDirectory + '/' + catagory + '.json'
         if not op.isfile(self.path + '/' + catagoryPath) :
             with open(self.path + '/' + catagoryPath , "w") as cat:
                 cat.write('{"models" : []}')
             self.GitHandler.AddNewFile(catagoryPath)
             catJson = {}
-            with open(self.path + '/' + self.catagoriesPath , "r") as cat:
+            with open(self.path + '/' + self.__catagoriesPath , "r") as cat:
                 catJson = json.loads(cat.read())
                 catJson["catagories"].append(catagory)
                 catJson["catagories"].sort()
-            with open(self.path + '/' + self.catagoriesPath , "w") as cat:
+            with open(self.path + '/' + self.__catagoriesPath , "w") as cat:
                 cat.write(json.dumps(catJson , indent=4))
-            self.GitHandler.CommitFile(self.catagoriesPath , "NEW CATAGORY ADDED")
+            self.GitHandler.CommitFile(self.__catagoriesPath , "NEW CATAGORY ADDED")
             return True
         return False
 
     def UpdateCatagory(self , catagory : str , models : list) -> bool:
-        catagoryPath = self.catagoriesDirectory + '/' + catagory + '.json'
+        catagoryPath = self.__catagoriesDirectory + '/' + catagory + '.json'
         if op.isfile(self.path + '/' + catagoryPath) : 
             catJson = {}
             with open(self.path + '/' + catagoryPath , "r") as cat:
